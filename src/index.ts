c7eb17fe31660c2a9c0160564251f82fedfae238
nugaon <toth.viktor.levente@gmail.com>
2020-05-25 21:03:41 +0200

    add contract serialization functions restructure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import EthCrypto, { Encrypted } from "eth-crypto";
+import { hexToBytes } from "web3-utils"
 
 export type IdentityType = {
   privateKey: string,
@@ -45,17 +46,58 @@ export function createIdentity(): IdentityType {
   return EthCrypto.createIdentity();
 }
 
-export function encryptData(publicKey: string, data: string): Promise<Encrypted> {
+export function encryptData(pubKey: string, data: string): Promise<Encrypted> {
   return EthCrypto.encryptWithPublicKey(
-    publicKey,
+    pubKey,
     data // message
   );
 }
 
-export function decryptData(privateKey: string, encryptedData: Encrypted): Promise<string> {
-  return EthCrypto.decryptWithPrivateKey(privateKey, encryptedData);
+export function decryptData(privKey: string, encryptedData: Encrypted): Promise<string> {
+  return EthCrypto.decryptWithPrivateKey(privKey, encryptedData);
 }
 
-export function compressPublicKey(publicKey: string): string {
-  return EthCrypto.publicKey.compress(publicKey);
+export function compressPublicKey(pubKey: string): string {
+  return EthCrypto.publicKey.compress(pubKey);
+}
+
+/// @dev Gives back the user's compressed public key first two characters in a boolean,
+/// because it only can be "02" or "03"
+/// returns true if "03"
+export function compressPublicKeyPrefix(pubKey: string): boolean {
+  const firstTwoChars: string = pubKey.substr(0, 2)
+  if(firstTwoChars === "03") {
+    return true
+  }
+  if(firstTwoChars === "02") {
+    return false
+  }
+  // else
+  throw new Error(`The first two characters of the ${pubKey}`
+    + ` compressed public key are not 02 or 03`)
+}
+
+export function decompressPublicKeyPrefix(prefix: boolean): string {
+  return prefix ? "03" : "02";
+}
+
+/// @dev at "subscribe" action the user can upload his key.
+export function serializePublicKey(
+  pubKey: string
+): { pubKeyPrefix: boolean, pubKey: Array<number> } {
+  let userPubKeyPart1: boolean = compressPublicKeyPrefix(pubKey);
+  let userPubKeyPart2: string = pubKey.substr(2, pubKey.length);
+  const userPubKey2 = hexToBytes("0x" + userPubKeyPart2);
+  return {
+    pubKeyPrefix: userPubKeyPart1,
+    pubKey: userPubKey2
+  }
+}
+
+/// @dev get compressed public key string from contract store
+export function unserializePublicKey(
+  pubKeyPrefix: boolean,
+  pubKey: string
+): string {
+  return decompressPublicKeyPrefix(pubKeyPrefix) + pubKey.substr(2, pubKey.length);
 }
